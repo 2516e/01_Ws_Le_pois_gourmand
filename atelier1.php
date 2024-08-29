e5b1126a12c8aa3f7b46539e6de806fd2b247037
2516e <mahleb.enzo@gmail.com>
2024-08-29 16:55:07 +0200

    devoir

---
+++ atelier1.php
@@ -0,0 +1,70 @@
+<div class="container">
+    <h1><i class="fa-solid fa-kiwi-bird fa-l"></i> Au Pois Gourmand</h1>
+    <div class="wrapper">
+
+        <div class="content">
+
+            <div class="img">
+                <img src="assets/img/rome.jpg" alt="">
+            </div>
+            <h2>Formule Rome</h2>
+            <p>Tomates buratina</p>
+            <p>Rizotto aux asperges</p>
+            <p>Panna cotta</p>
+            <div class="button">
+                <button class="btn rome">
+                    Choisir
+                </button>
+            </div>
+        </div>
+        
+        <div class="content">
+
+            <div class="img">
+                <img src="assets/img/nyork.jpg" alt="">
+            </div>
+            <h2>Formule New-York</h2>
+            <p>César salade</p>
+            <p>Cheese burger</p>
+            <p>Cheesecake</p>
+            <div class="button">
+                <button class="btn nyork">
+                    Choisir
+                </button>
+            </div>
+        </div>
+
+        <div class="content">
+
+            <div class="img">
+                <img src="assets/img/delhi.jpg" alt="">
+            </div>
+            <h2>Formule Delhi</h2>
+            <p>Poppadoms</p>
+            <p>Agneau byriani</p>
+            <p>Lassi mangue</p>
+            <div class="button">
+                <button class="btn delhi">
+                    Choisir
+                </button>
+            </div>
+        </div>
+
+        <div class="content">
+
+            <div class="img">
+                <img src="assets/img/hanoi.jpg" alt="">
+            </div>
+            <h2>Formule Hanoï</h2>
+            <p>Nems aux crevettes</p>
+            <p>Poulet saté </p>
+            <p>Perles de coco</p>
+            <div class="button">
+                <button class="btn hanoi">
+                    Choisir
+                </button>
+            </div>
+        </div>
+
+    </div>
+</div>
